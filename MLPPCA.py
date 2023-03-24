cdd4a5743b83ca0f2b88fe0dc3b32d7e76bed958
TRABELSI Youssef <youssef.trabelsi@telecomnancy.eu>
2023-03-24 10:37:57 +0100

    hihihi

--- MLPPCA.py
+++ MLPPCA.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 # Load the data
 data = pd.read_csv('data/PCA_transformed_data.csv')
-#print(data)
+print(data)
 # Split the data into features and target
-X = data.iloc[:, :-1].values
-y = data.iloc[:, -1].values.astype(int)
+X = data.iloc[:, 1:4].values
+y = data.iloc[:, 0].values.astype(int)
 
 
 # Train the MLPClassifier on the data
@@ -21,8 +23,8 @@ new_data = pd.read_csv('data/PCA_transformed_well_data.csv')
 
 # Make sure the new data has the same length as the training data
 #new_data = new_data.iloc[:, :X.shape[1]]
-print(new_data.iloc[:,1:4])
 
 # Make predictions on the new data
 predictions = clf.predict(new_data.iloc[:,1:4])
-print(predictions)
+print(confusion_matrix(new_data.iloc[:,0], predictions))
+print(accuracy_score(new_data.iloc[:,0], predictions))
